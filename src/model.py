9de3a30a34dbdc421f8d1a466f7aaed9f357e16c
Josefina Urquiza <josefina.urquizap@gmail.com>
2026-08-18 15:18:04 -0300

    archivos en src se actualizan, tambien se prueba pipeline de modelos AU

---
+++ src/model.py
@@ -0,0 +1,51 @@
+
+
+# Se aplica el modelo a la fecha de interes, pero hay un lag minimo de 10 dias
+
+# Setear informacion de entrada
+# ---------------------------------------------------------
+# 1. Se abre AOD
+
+
+# ---------------------------------------------------------
+# 2. Se abre NDVI
+
+
+# ---------------------------------------------------------
+# 3. Se abre Variables metereologicas
+
+
+
+
+# ---------------------------------------------------------
+# 4. Se abre Composicion de aerosoles
+
+
+
+
+# ---------------------------------------------------------
+# 5. Se abre Elevacion
+
+
+
+
+# ---------------------------------------------------------
+# 6. Se abre Dia
+
+
+# ---------------------------------------------------------
+# Se genera un stack de datos
+
+
+# ---------------------------------------------------------
+# Se abre el modelo previamente entrenado y validado
+
+
+
+# ---------------------------------------------------------
+# Se aplica el modelo al stack de datos con todas las variables
+
+
+
+# ---------------------------------------------------------
+# Se guarda la imagen a nivel local
